cb11a1ce851ab19c4b4a3c3a9c8261e0a0d17a10
Danilo Santos <dscofield@outlook.com.br>
2024-09-28 12:42:10 -0300

    feat: add separator between tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,6 +2,9 @@ import Button from './Button';
 
 import AddIcon from '../assets/icons/Add.svg?react';
 import TrashIcon from '../assets/icons/Trash.svg?react';
+import SunIcon from '../assets/icons/sun.svg?react';
+import CloudIcon from '../assets/icons/cloud-sun.svg?react';
+import MoonIcon from '../assets/icons/moon.svg?react';
 
 const Tasks = () => {
   return (
@@ -24,6 +27,38 @@ const Tasks = () => {
             Nova tarefas
             <AddIcon />
           </Button>
+        </div>
+      </div>
+
+      <div className="rounded-xl bg-white p-6">
+        {/* manha */}
+        <div className="space-y-3">
+          <div className="flex items-center gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <SunIcon />
+            <p className="text-sm text-[#9A9C9F]">Manhã</p>
+          </div>
+
+          {/* tasks */}
+        </div>
+
+        {/* Tarde */}
+        <div className="my-6 space-y-3">
+          <div className="flex items-center gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <CloudIcon />
+            <p className="text-sm text-[#9A9C9F]">Tarde</p>
+          </div>
+
+          {/* tasks */}
+        </div>
+
+        {/* noite */}
+        <div className="space-y-3">
+          <div className="flex items-center gap-2 border-b border-solid border-[#f4f4f5] pb-1">
+            <MoonIcon />
+            <p className="text-sm text-[#9A9C9F]">Noite</p>
+          </div>
+
+          {/* tasks */}
         </div>
       </div>
     </div>
